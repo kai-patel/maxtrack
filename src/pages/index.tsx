7304b90d35e4941eb725b2105cb380503c8e3f6f
Kai Patel <00kaipatel@gmail.com>
2022-08-20 20:50:15 +0100

    Add sign out

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -16,23 +16,38 @@ const Home: NextPage = () => {
   return (
     <div className="h-screen max-h-screen">
       <header>
-        <nav className="sticky top-0 flex bg-gray-600 min-w-screen p-4 h-[5%] justify-between items-center">
-          <p className="font-bold text-gray-100 select-none">MaxTrack</p>
-          {status === "authenticated" ? (
-            <button
-              className="text-gray-100 justify-end hover:underline"
-              onClick={() => signOut()}
-            >
-              Sign Out
-            </button>
-          ) : (
-            <button
-              className="text-gray-100 justify-end hover:underline"
-              onClick={() => signIn()}
-            >
-              Login
-            </button>
-          )}
+        <nav className="sticky top-0 flex bg-gray-600 min-w-screen p-4 h-[5%] justify-between items-center shadow">
+          <p className="font-bold text-gray-100 select-none text-lg">
+            MaxTrack
+          </p>
+          <div className="flex">
+            {status === "authenticated" ? (
+              <>
+                <button className="flex text-gray-100 justify-end items-center hover:underline">
+                  <p className="text-gray-100">{session.user?.name}</p>
+                  <img
+                    className="aspect-square object-scale-down mr-4 ml-4 rounded-full"
+                    src={session.user?.image || ""}
+                    height="32"
+                    width="32"
+                  />
+                </button>
+                <button
+                  className="text-gray-100 justify-end hover:underline"
+                  onClick={() => signOut()}
+                >
+                  Sign Out
+                </button>
+              </>
+            ) : (
+              <button
+                className="text-gray-100 justify-end hover:underline"
+                onClick={() => signIn()}
+              >
+                Login
+              </button>
+            )}
+          </div>
         </nav>
       </header>
       <main className="relative container mx-auto flex flex-col items-center justify-center max-h-[95%] p-4">
