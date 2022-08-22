49c7776ba249c47555512833946e18001d6a2621
Kai Patel <00kaipatel@gmail.com>
2022-08-22 19:25:28 +0100

    Cancel query if no session

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -121,13 +121,15 @@ function Dashboard({ session, status }: DashboardProps) {
     overhead: 0,
   });
 
-  if (status !== "authenticated" || !session)
+  if (status !== "authenticated" || !session) {
+    utils.cancelQuery(["lifts.getAll"]);
     return (
       <p className="font-bold text-2xl text-center w-full h-full p-10 bg-gray-100">
         You are not logged in. Whether you are a new or an existing user, please
         sign in to use this website.
       </p>
     );
+  }
 
   return (
     <div className="flex flex-col w-full h-full bg-gray-800">
