78cba10222a7a9d9f9079c3e1d1fca7653d1eac2
Kai Patel <00kaipatel@gmail.com>
2022-08-24 19:14:10 +0100

    Add Dashboard button, highlight img on hover

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -6,7 +6,12 @@ export function NavBar({ session, status }: NavBarProps) {
   return (
     <nav className="flex bg-gray-600 min-w-screen m-0 p-4 h-[5%] justify-between items-center shadow">
       <p className="font-bold text-gray-100 select-none text-lg">MaxTrack</p>
-      <NavBarProfileButton session={session} status={status} />
+      <span className="flex items-center">
+        <p className="text-gray-100 select-none hover:underline p-4">
+          Dashboard
+        </p>
+        <NavBarProfileButton session={session} status={status} />
+      </span>
     </nav>
   );
 }
@@ -17,10 +22,12 @@ function NavBarProfileButton({ session, status }: NavBarProps) {
     <div className="flex">
       {status === "authenticated" && session ? (
         <>
-          <button className={`flex items-center ${buttonStyle}`}>
+          <button
+            className={`flex items-center ${buttonStyle} [&>img]:hover:border-2 [&>img]:hover:border-emerald-500`}
+          >
             <p className="text-gray-100">{session.user?.name}</p>
             <img
-              className="aspect-square object-scale-down mr-4 ml-4 rounded-full"
+              className="aspect-square object-scale-down mr-4 ml-4 rounded-full "
               src={session.user?.image || ""}
               height="32"
               width="32"
