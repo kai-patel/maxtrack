3fcfbd237eb26bc5c1e8e81ea595955d017b6594
Kai Patel <00kaipatel@gmail.com>
2022-08-24 21:58:32 +0100

    Add profile page

---
+++ src/pages/profile.tsx
@@ -0,0 +1,27 @@
+import _ from "lodash";
+import { NextPage } from "next";
+import { useSession } from "next-auth/react";
+import { NavBar } from "../components/NavBar";
+
+const Profile: NextPage = () => {
+  const { data: session, status } = useSession();
+  if (status !== "authenticated" || !session || !session.user) return null;
+
+  return (
+    <div className="h-screen max-h-screen overflow-hidden m-0 p-0">
+      <header className="sticky top-0">
+        <NavBar session={session} status={status} />
+      </header>
+      <main className="relative flex flex-col items-center h-[95%] bg-gray-200">
+        <div className="flex flex-col w-full h-full bg-gray-800">
+          <div className="flex flex-row flex-shrink h-fit bg-gray-100 border border-gray-900 shadow rounded m-4 p-4 justify-evenly items-center">
+            <p>{`Name: ${session.user.name}`}</p>
+            <img src={session.user.image || ""} height="128px" width="128px" />
+          </div>
+        </div>
+      </main>
+    </div>
+  );
+};
+
+export default Profile;
