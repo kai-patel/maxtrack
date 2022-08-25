c5964d725c682f2b4bc66e24e1ec19d82741fd6c
Kai Patel <00kaipatel@gmail.com>
2022-08-25 17:17:20 +0100

    Fix Link styling

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -8,11 +8,10 @@ export function NavBar({ session, status }: NavBarProps) {
     <nav className="flex bg-gray-600 min-w-screen m-0 px-4 h-[5%] min-h-[5%] max-h-[5%] justify-between items-center shadow">
       <p className="font-bold text-gray-100 select-none text-lg">MaxTrack</p>
       <span className="flex items-center">
-        <Link
-          className="text-gray-100 select-none hover:underline p-4"
-          href="/"
-        >
-          Dashboard
+        <Link href="/">
+          <a className="text-gray-100 select-none hover:underline p-4">
+            Dashboard
+          </a>
         </Link>
         <NavBarProfileButton session={session} status={status} />
       </span>
@@ -26,18 +25,19 @@ function NavBarProfileButton({ session, status }: NavBarProps) {
     <div className="flex">
       {status === "authenticated" && session ? (
         <>
-          <Link
-            className={`flex items-center ${buttonStyle} [&>img]:hover:border-2 [&>img]:hover:border-emerald-500`}
-            href="/profile"
-          >
-            <p className="text-gray-100">{session.user?.name}</p>
-            <img
-              className="aspect-square object-scale-down mr-4 ml-4 rounded-full "
-              src={session.user?.image || ""}
-              height="32"
-              width="32"
-              referrerPolicy="no-referrer"
-            />
+          <Link href="/profile">
+            <a
+              className={`flex items-center ${buttonStyle} [&>img]:hover:border-2 [&>img]:hover:border-emerald-500`}
+            >
+              <p className="text-gray-100">{session.user?.name}</p>
+              <img
+                className="aspect-square object-scale-down mr-4 ml-4 rounded-full "
+                src={session.user?.image || ""}
+                height="32"
+                width="32"
+                referrerPolicy="no-referrer"
+              />
+            </a>
           </Link>
           <button className={buttonStyle} onClick={() => signOut()}>
             Sign Out
